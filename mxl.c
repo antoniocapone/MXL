9a6b703d2e88195979ae7b7ec123bb79e5c3c8eb
Antonio Capone <antonino2002d@gmail.com>
2025-03-02 19:30:59 +0100

    Added Matrix_Init Added Matrix_Identity Added Matrix_Zeros Added Matrix_Free Added Matrix_Euclidean_Norm Added Matrix_FPrint Added Matrix_Copy Added Matrix_Add Added Matrix_Sub Added Matrix_Multiply Added Matrix_Scalar_Multiply

--- mxl.c
+++ mxl.c
@@ -1,1 +1,161 @@
 #include "mxl.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+Matrix_Op_Result Matrix_Init(Matrix *matrix, const int r, const int c) {
+	matrix->rows = r;
+	matrix->cols = c;
+	matrix->raw = malloc(r * c * sizeof(scalar));
+	if (matrix->raw != NULL) {
+		return OP_MEMORY_ERROR;
+	}
+
+	return OP_OK;
+}
+
+Matrix_Op_Result Matrix_Identity(Matrix *m, const int r, const int c) {
+	Matrix_Op_Result res = Matrix_Init(m, r, c);
+	ASSERT_STATUS(res);
+
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			if (i == j) {
+				MAT_ELEM(m, i, j) = 1;
+			} else {
+				MAT_ELEM(m, i, j) = 0;
+			}
+		}
+	}
+}
+
+Matrix_Op_Result Matrix_Zeros(Matrix *m, const int r, const int c) {
+	Matrix_Op_Result res = Matrix_Init(m, r, c);
+	ASSERT_STATUS(res);
+
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			MAT_ELEM(m, i, j) = 0;
+		}
+	}
+}
+
+void Matrix_Free(Matrix *matrix) {
+	if (matrix->raw != NULL) {
+		free(matrix->raw);
+	}
+}
+
+Matrix_Op_Result Matrix_Euclidean_Norm(Matrix *vector, scalar *norm) {
+	if (vector->cols != 1 && vector->rows != 1) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	if (vector->cols == 1) {
+		for (int i = 0; i < vector->rows; i++) {
+			*norm += powf(MAT_ELEM(vector, i, 1), 2.0f);
+		}
+		*norm = sqrtf(*norm);
+	}
+
+	if (vector->rows == 1) {
+		for (int j = 0; j < vector->cols; j++) {
+			*norm += powf(MAT_ELEM(vector, 1, j), 2.0f);
+		}
+		*norm = sqrtf(*norm);
+	}
+
+	return OP_OK;
+}
+
+void Matrix_FPrint(FILE *stream, Matrix *m) {
+	fprintf(stream, "[\t");
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			fprintf(stream, "%f,", MAT_ELEM(m, i, j));
+		}
+		fprintf(stream, "\n");
+	}
+	fprintf(stream, "]");
+}
+
+Matrix_Op_Result Matrix_Copy(Matrix *src, Matrix *dst) {
+	if (src->rows != dst->rows || src->cols != dst->cols) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	for (int i = 0; i < dst->rows; i++) {
+		for (int j = 0; j < dst->cols; j++) {
+			MAT_ELEM(dst, i, j) = MAT_ELEM(src, i, j);
+		}
+	}
+
+	return OP_OK;
+}
+
+Matrix_Op_Result Matrix_Add(Matrix *m1, Matrix *m2, Matrix *dst) {
+	if (m1->rows != m2->rows || m1->cols != m2->cols || m1->rows != dst->rows || m1->cols != dst->cols) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	for (int i = 0; i < m1->rows; i++) {
+		for (int j = 0; j < m1->cols; j++) {
+			MAT_ELEM(dst, i, j) = MAT_ELEM(m1, i, j) + MAT_ELEM(m2, i, j);
+		}
+	}
+
+	return OP_OK;
+}
+
+Matrix_Op_Result Matrix_Sub(Matrix *m1, Matrix *m2, Matrix *dst) {
+	if (m1->rows != m2->rows || m1->cols != m2->cols || m1->rows != dst->rows ||
+		m1->cols != dst->cols) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	for (int i = 0; i < m1->rows; i++) {
+		for (int j = 0; j < m1->cols; j++) {
+			MAT_ELEM(dst, i, j) = MAT_ELEM(m1, i, j) - MAT_ELEM(m2, i, j);
+		}
+	}
+
+	return OP_OK;
+}
+
+Matrix_Op_Result Matrix_Multiply(Matrix *m1, Matrix *m2, Matrix *dst) {
+	/* Check for m1 and m2 product compatibility */
+	if (m1->cols != m2->rows) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	/* (n, m) dot (m, p) ===> (n, p) */
+	if (dst->rows != m1->rows || dst->cols != m2->cols) {
+		return OP_DIMENSION_MISMATCH;
+	}
+
+	for (int i = 0; i < dst->rows; i++) {
+		for (int j = 0; j < dst->cols; j++) {
+			MAT_ELEM(dst, i, j) = Matrix_Dot_RowCol(m1, i, m2, j);
+		}
+	}
+
+	return OP_OK;
+}
+
+static scalar Matrix_Dot_RowCol(Matrix *m1, const int row_m1, Matrix *m2, const int col_m2) {
+	scalar res = 0;
+	/* Assume that m1 and m2 are both squared and same order */
+	for (int i = 0; i < m1->rows; i++) {
+		res += MAT_ELEM(m1, row_m1, i) * MAT_ELEM(m2, i, col_m2);
+	}
+
+	return res;
+}
+
+void Matrix_Scalar_Multiply(Matrix *m, scalar s) {
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			MAT_ELEM(m, i, j) = s * MAT_ELEM(m, i, j);
+		}
+	}
+}
--- mxl.h
+++ mxl.h
@@ -9,27 +9,29 @@ typedef struct {
 	scalar *raw;
 } Matrix;
 
-#define MAT_ELEM(m, i, j) (m.raw[(i) * m.cols + (j)])
+#define MAT_ELEM(m, i, j) (m->raw[(i) * m->cols + (j)])
+#define ASSERT_STATUS(status) if (status != OP_OK) return status;
 
 typedef enum {
 	OP_OK,
 	OP_NOT_OK,
 	OP_DIMENSION_MISMATCH,
-	OP_SINGULAR_MATRIX
+	OP_SINGULAR_MATRIX,
+	OP_MEMORY_ERROR
 } Matrix_Op_Result;
 
-void Matrix_Init(Matrix *matrix, const int r, const int c);
+Matrix_Op_Result Matrix_Init(Matrix *matrix, const int r, const int c);
+Matrix_Op_Result Matrix_Identity(Matrix *m, const int r, const int c);
+Matrix_Op_Result Matrix_Zeros(Matrix *m, const int r, const int c);
 void Matrix_Free(Matrix *matrix);
 
 Matrix_Op_Result Matrix_Euclidean_Norm(Matrix *vector, scalar *norm);
 
-void Matrix_Print(Matrix *m);
-void Matrix_Copy(Matrix *src, Matrix *dst);
-void Matrix_Identity(Matrix *m);
-void Matrix_Zeros(Matrix *m);
-void Matrix_Add(Matrix *m1, Matrix *m2);
-void Matrix_Sub(Matrix *m1, Matrix *m2);
-Matrix_Op_Result Matrix_Multiply(Matrix *m1, Matrix *m2);
+void Matrix_FPrint(FILE *stream, Matrix *m);
+Matrix_Op_Result Matrix_Copy(Matrix *src, Matrix *dst);
+Matrix_Op_Result Matrix_Add(Matrix *m1, Matrix *m2, Matrix *dst);
+Matrix_Op_Result Matrix_Sub(Matrix *m1, Matrix *m2, Matrix *dst);
+Matrix_Op_Result Matrix_Multiply(Matrix *m1, Matrix *m2, Matrix *dst);
 void Matrix_Scalar_Multiply(Matrix *m, scalar s);
 void Matrix_Transpose(Matrix *src, Matrix *dst);
 Matrix_Op_Result Matrix_Rank(Matrix *m, int *rank);
